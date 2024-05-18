a55db8d90023219ccf2025302511703ecb49b00b
Gabriel Bustos <Gbustosgarcia01@gmail.com>
2024-05-18 14:42:44 -0300

    Done

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let sections = document.querySelectorAll('section');
 window.onscroll = () => {
   sections.forEach(sec => {
     let top = window.scrollY;
-    let offset = sec.offsetTop;
+    let offset = sec.offsetTop - 600;
     let height = sec.offsetHeight;
 
     if (top >= offset && top < offset + height) {
